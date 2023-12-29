734c998df5bc877381bf09505a5738bcf0d82b87
raspacee <khapungbj84@gmail.com>
2023-12-29 14:39:00 +0545

    added logging

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +12,32 @@ const userRouter = require("./routes/user.js");
 const productRouter = require("./routes/product.js");
 const adminRouter = require("./routes/admin.js");
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+const errorLogStream = fs.createWriteStream(path.join(__dirname, "error.log"), {
+  flags: "a",
+});
+
+// Log all errors to error.log
+app.use(
+  morgan("combined", {
+    stream: errorLogStream,
+    skip: function (req, res) {
+      return res.statusCode < 400;
+    },
+  })
+);
+app.use(
+  morgan("common", {
+    stream: accessLogStream,
+    skip: function (req, res) {
+      return res.statusCode >= 400;
+    },
+  })
+);
 app.use(cors());
 app.use(express.json());
 
